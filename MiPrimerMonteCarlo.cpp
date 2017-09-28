3b42b9c968a80f02f13f5fc6333637e609f70feb
Juansebae <31037176+Juansebae@users.noreply.github.com>
2017-09-28 09:41:06 -0500

    Programa modificado para trabajar con Agentes Econ

--- MiPrimerMonteCarlo.cpp
+++ MiPrimerMonteCarlo.cpp
@@ -4,10 +4,68 @@
 #include "Random64.h"
 using namespace std;
 
+const int N=1000;
+
+
+class Agente{
+private:
+  double dinero;
+public:
+  void Inicie(double dinero0){dinero=dinero0;};
+  double Getdinero(void){return dinero;};
+  friend class Mercado;
+};
+
+class Mercado{
+private:
+public:
+  void HagaTransaccionEntre(Agente & Agente1,Agente & Agente2,Crandom & ran64);
+};
+
+void Mercado::HagaTransaccionEntre(Agente & Agente1,Agente & Agente2,Crandom & ran64){
+  double GananciaAgente1,GananciaAgente2,Resultado;
+  double NuevoDineroAgente1,NuevoDineroAgente2;
+  Resultado=2*ran64.r();
+  GananciaAgente1=Resultado-1;
+  GananciaAgente2=(2-Resultado)-1;
+  NuevoDineroAgente1=Agente1.dinero+GananciaAgente1;
+  NuevoDineroAgente2=Agente2.dinero+GananciaAgente2;
+  if(NuevoDineroAgente1>0 && NuevoDineroAgente2>0){
+  Agente1.dinero=NuevoDineroAgente1;
+  Agente2.dinero=NuevoDineroAgente2;
+  }
+
+}
+
+
+
 int main(void){
+  Mercado Corabastos;
+  Agente Paisano[N];
   Crandom ran64(1);
-  int i; double mu=3,sigma=2;
-  for(i=0;i<1e4;i++)
-    cout<<sigma*sqrt(-2*log(ran64.r()))*cos(2*M_PI*ran64.r())+mu<<endl;
+  int i,j,t,Ntransacciones=N*10000;
+  // Inicie todos los Agentes con 10 pesos
+  for(i=0;i<N;i++){Paisano[i].Inicie(10);}
+  
+  for(t=0;t<Ntransacciones;t++){
+
+    // Escogo dos agentes al azar.
+    i=(int)N*ran64.r();
+    j=(int)(N-1)*ran64.r(); if(j>=i){j++;}
+    
+    // Los pongo a interactuar.
+    Corabastos.HagaTransaccionEntre(Paisano[i],Paisano[j],ran64);
+
+    
+
+
+
+
+  }
+
+  // Imprimo el dinero de todos.
+  for(i=0;i<N;i++){cout<<Paisano[i].Getdinero()<<endl;}
+  
+  
   return 0;
 }
